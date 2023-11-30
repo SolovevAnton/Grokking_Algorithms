d913f74677a55bea7c40d87e8c1116de67b261fa
Solovev Anton <solovev.anton93@gmail.com>
2023-11-30 11:53:07 +0100

    Insertion sort impl and tested

---
+++ src/main/java/com/solovev/sort/InsertionSort.java
@@ -0,0 +1,28 @@
+package com.solovev.sort;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class InsertionSort<T extends Comparable<T>> implements Sort<T> {
+    private List<T> result;
+    private int currentIndex;
+
+    @Override
+    public List<T> sort(List<T> unsorted) {
+        result = new ArrayList<>(unsorted);
+        int size = unsorted.size();
+        for (int i = 0; i < size; i++) {
+            currentIndex = i;
+            while (currentIndex > 0 && isCurrentSmaller()) {
+                Collections.swap(result,currentIndex - 1,currentIndex);
+                currentIndex--;
+            }
+        }
+        return result;
+    }
+
+    private boolean isCurrentSmaller() {
+        return result.get(currentIndex).compareTo(result.get(currentIndex-1)) < 0;
+    }
+}
