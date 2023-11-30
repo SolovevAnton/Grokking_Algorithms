ecf58fcfee4b6cf4e200a062933f88675bfa2c6a
Solovev Anton <solovev.anton93@gmail.com>
2023-11-30 11:14:30 +0100

    Selection sort impl and tested

---
+++ src/main/java/com/solovev/sort/SelectionSort.java
@@ -0,0 +1,30 @@
+package com.solovev.sort;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SelectionSort<T extends Comparable<T>> implements Sort<T> {
+    private List<T> result;
+    private int minIndex;
+
+    @Override
+    public List<T> sort(List<T> unsorted) {
+        result = new ArrayList<>(unsorted);
+        int size = unsorted.size();
+        for (int i = 0; i < size; i++) {
+            minIndex = i;
+            for (int j = size - 1; j > i; j--) {
+                if (isSmallerThanMin(j)) {
+                    minIndex = j;
+                }
+            }
+            Collections.swap(result,i,minIndex);
+        }
+        return result;
+    }
+
+    private boolean isSmallerThanMin(int indexOfElement) {
+        return result.get(minIndex).compareTo(result.get(indexOfElement)) > 0;
+    }
+}
---
+++ src/test/java/com/solovev/sort/SelectionSortTest.java
@@ -0,0 +1,65 @@
+package com.solovev.sort;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SelectionSortTest {
+    private final Sort<Integer> sortClass = new SelectionSort<>();
+
+    @Test
+    void testSortWithEmptyList() {
+        List<Integer> unsorted = Collections.emptyList();
+        List<Integer> sorted = sortClass.sort(unsorted);
+        assertEquals(unsorted, sorted);
+    }
+
+    @Test
+    void testSortWithOneElement() {
+        List<Integer> unsorted = List.of(5);
+        List<Integer> sorted = sortClass.sort(unsorted);
+        assertEquals(unsorted, sorted);
+    }
+
+    @Test
+    void testSortWithMultipleElements() {
+        List<Integer> unsorted = List.of(5, 2, 8, 1, 3);
+        List<Integer> sorted = sortClass.sort(unsorted);
+        List<Integer> expected = Arrays.asList(1, 2, 3, 5, 8);
+        assertEquals(expected, sorted);
+    }
+
+    @Test
+    void testSortWithAlreadySorted() {
+        List<Integer> unsorted = List.of(1, 2, 3, 5, 8);
+        List<Integer> sorted = sortClass.sort(unsorted);
+        assertEquals(unsorted, sorted);
+    }
+
+    @Test
+    void testSortWithDescendingOrder() {
+        List<Integer> unsorted = List.of(8, 5, 3, 2, 1);
+        List<Integer> sorted = sortClass.sort(unsorted);
+        List<Integer> expected = List.of(1, 2, 3, 5, 8);
+        assertEquals(expected, sorted);
+    }
+    @Test
+    void testSortWithRepeatedElements() {
+        List<Integer> unsorted = List.of(5, 2, 8, 1, 3, 2, 5);
+        List<Integer> sorted = sortClass.sort(unsorted);
+        List<Integer> expected = Arrays.asList(1, 2, 2, 3, 5, 5, 8);
+        assertEquals(expected, sorted);
+    }
+
+    @Test
+    void testSortWithAllSameElements() {
+        List<Integer> unsorted = List.of(4, 4, 4, 4, 4);
+        List<Integer> sorted = sortClass.sort(unsorted);
+        assertEquals(unsorted, sorted);
+    }
+
+}
